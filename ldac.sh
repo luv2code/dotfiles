429e8d0a18cfca6152fa5df71236eddadfbcae02
Matthew Taylor <luv2code@gmail.com>
2023-04-11 19:04:11 -0500

    expanded codec changer script to handle case where no change is necessary

--- ldac.sh
+++ ldac.sh
@@ -1,3 +1,22 @@
 #! /usr/bin/env zsh
+CODEC=`pacmd send-message /card/bluez_card.F8_4E_17_55_4B_EF/bluez get-codec | sed s/\"//g`
 
-pactl send-message /card/bluez_card.F8_4E_17_55_4B_EF/bluez switch-codec "ldac_$1"
+if [[ "$1" == *q && "$1" != ldac_* ]]; then 
+  NEW_CODEC="ldac_$1"
+else
+  NEW_CODEC="$1"
+fi
+
+if [[ "$NEW_CODEC" == "$CODEC" ]]; then
+  echo "$NEW_CODEC already set"
+  exit 0
+else
+  echo "set $NEW_CODEC"
+fi
+
+pactl send-message /card/bluez_card.F8_4E_17_55_4B_EF/bluez switch-codec $NEW_CODEC
+
+if [[ $? -ne 0 ]]; then 
+  echo "choose from:"
+  pacmd send-message /card/bluez_card.F8_4E_17_55_4B_EF/bluez list-codecs | jq -r '.[].name'
+fi
