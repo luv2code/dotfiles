106bda87abfa618aa3dfd14b5b051edaef000995
Matthew Taylor <luv2code@gmail.com>
2023-10-24 22:40:25 -0500

    added wezterm to wezterm window titles to make grabbing a handle to it easier

--- home/.wezterm.lua
+++ home/.wezterm.lua
@@ -104,5 +104,18 @@ wezterm.on("gui-startup", function(cmd)
   window:gui_window():maximize()
 end)
 
+wezterm.on('format-window-title', function(tab, pane, tabs, panes, config)
+  local zoomed = ''
+  if tab.active_pane.is_zoomed then
+    zoomed = '[Z] '
+  end
+
+  local index = ''
+  if #tabs > 1 then
+    index = string.format('[%d/%d] ', tab.tab_index + 1, #tabs)
+  end
+	return zoomed .. index .. tab.active_pane.title .. ' wezterm'
+end)
+
 -- and finally, return the configuration to wezterm
 return config
