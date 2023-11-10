c288ec583de0de023cf0954290dc63d5815684d2
Matthew Taylor <luv2code@gmail.com>
2023-11-09 20:38:04 -0600

    wezterm: added leader key toggle and last pane switching

--- home/.wezterm.lua
+++ home/.wezterm.lua
@@ -12,9 +12,9 @@ if wezterm.config_builder then
 end
 
 -- This is where you actually apply your config choices
-
 -- For example, changing the color scheme:
 config.color_scheme = 'Tomorrow Night Bright (Gogh)'
+config.colors = { background = "black" }
 config.window_background_opacity = 0.85
 config.font_size = 16
 config.scrollback_lines = 20000
@@ -24,7 +24,6 @@ config.tab_bar_at_bottom = true
 
 config.show_update_window = true
 config.check_for_updates = true
-
 -- timeout_milliseconds defaults to 1000 and can be omitted
 config.leader = { key = 'a', mods = 'CTRL', timeout_milliseconds = 1000 }
 config.keys = {
@@ -81,14 +80,40 @@ config.keys = {
   {
     key = ';',
     mods = 'LEADER',
-    action = act.ActivateLastTab,
+		action = wezterm.action {EmitEvent = "activate-last-pane"},
   },
-  -- Send "CTRL-A" to the terminal when pressing CTRL-A, CTRL-A
+  -- Tab navigation
   {
     key = 'a',
     mods = 'LEADER|CTRL',
-    action = act.SendString '\x01',
+    action = act.ActivateLastTab,
   },
+  {
+    key = '1',
+    mods = 'LEADER',
+    action = act.ActivateTab(0),
+  },
+  {
+    key = '2',
+    mods = 'LEADER',
+    action = act.ActivateTab(1),
+  },
+  {
+    key = '3',
+    mods = 'LEADER',
+    action = act.ActivateTab(2),
+  },
+  {
+    key = '4',
+    mods = 'LEADER',
+    action = act.ActivateTab(3),
+  },
+	-- Turn off leader key for nested tmux sessions
+	{
+		key = 'F12',
+		mods = 'NONE',
+		action = wezterm.action {EmitEvent = "toggle-leader"},
+	},
 }
 
 -- maximize on startup
@@ -110,6 +135,46 @@ wezterm.on('format-window-title', function(tab, pane, tabs, panes, config)
     index = string.format('[%d/%d] ', tab.tab_index + 1, #tabs)
   end
 	return zoomed .. index .. tab.active_pane.title .. ' wezterm'
+end)
+
+wezterm.on("toggle-leader", function(window, pane)
+  local overrides = window:get_config_overrides() or {}
+  if not overrides.leader then
+    -- replace it with an "impossible" leader that will enver be pressed
+    overrides.leader = { key = "_", mods = "CTRL|ALT|SUPER" }
+		overrides.colors = { background = "#100000" }
+		overrides.window_background_opacity = 0.95
+    wezterm.log_warn("[leader] clear")
+  else
+    -- restore to the main leader
+    overrides.leader = nil
+		overrides.colors = nil
+		overrides.window_background_opacity = nil
+    wezterm.log_warn("[leader] set")
+  end
+  window:set_config_overrides(overrides)
+end)
+
+local last_active_by_tab = {}
+wezterm.on('format-tab-title', function(tab, tabs, panes, config, hover, max_width)
+	if tab.is_active and not last_active_by_tab[tab.tab_id] then
+		last_active_by_tab[tab.tab_id] = tab.active_pane.pane_id
+	end
+end)
+
+wezterm.on("activate-last-pane", function(window, pane)
+	if pane:tab() then
+		local tabid = pane:tab():tab_id()
+		local lastpaneid = last_active_by_tab[tabid]
+		local curpaneid = pane:pane_id()
+		for _, p in pairs(pane:tab():panes()) do
+			local paneid = p:pane_id()
+			if paneid == lastpaneid then
+				p:activate()
+				last_active_by_tab[tabid] = curpaneid
+			end
+		end
+	end
 end)
 
 -- and finally, return the configuration to wezterm
