fd76b825877503614527e4d9548a984bd7fc59d0
Matthew Taylor <luv2code@gmail.com>
2025-01-10 10:59:28 -0600

    adjusted ldac.sh for pipewire compat

--- ldac.sh
+++ ldac.sh
@@ -1,5 +1,5 @@
 #! /usr/bin/env zsh
-CODEC=`pacmd send-message /card/bluez_card.AC_80_0A_28_30_F1/bluez get-codec | sed s/\"//g`
+CODEC=`pactl send-message /card/bluez_card.AC_80_0A_28_30_F1/bluez get-codec | sed s/\"//g`
 
 if [[ "$1" == *q && "$1" != ldac_* ]]; then 
   NEW_CODEC="ldac_$1"
@@ -18,5 +18,5 @@ pactl send-message /card/bluez_card.AC_80_0A_28_30_F1/bluez switch-codec $NEW_CO
 
 if [[ $? -ne 0 ]]; then 
   echo "choose from:"
-  pacmd send-message /card/bluez_card.AC_80_0A_28_30_F1/bluez list-codecs | jq -r '.[].name'
+	pactl send-message /card/bluez_card.AC_80_0A_28_30_F1/bluez list-codecs | jq -r '.[] | "\(.name) \(.description)"'
 fi
