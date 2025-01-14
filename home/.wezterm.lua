dcab744cc7e2cc6208e9aef1a6f7eca7f2bd073d
Matthew Taylor <luv2code@gmail.com>
2025-01-14 11:43:16 -0600

    wezterm - thickened to the underline/undercurl

--- home/.wezterm.lua
+++ home/.wezterm.lua
@@ -5,11 +5,13 @@ local act = wezterm.action
 -- This table will hold the configuration.
 local config = {}
 
+
 -- In newer versions of wezterm, use the config_builder which will
 -- help provide clearer error messages
 if wezterm.config_builder then
   config = wezterm.config_builder()
 end
+
 
 config.term = 'wezterm'
 
@@ -28,6 +30,7 @@ config.window_padding = {
   top = 5,
   bottom = 5,
 }
+config.underline_thickness = '2'
 
 
 config.hide_tab_bar_if_only_one_tab = true
